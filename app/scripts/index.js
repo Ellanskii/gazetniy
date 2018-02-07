aa0dbe6593d7a3a7381848032da277e279f14e24
Elijah Ellanski <nameimprobable@gmail.com>
2018-02-08 00:57:01 +0300

    kinda done

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,5 +1,17 @@
+/* eslint-disable */
 import './../styles/variables-bulma.sass'
 import './../styles/main.scss'
+import './../styles/slick-carousel/slick/slick.scss';
+//import './../styles/slick-carousel/slick/slick-theme.scss';
+
+import $ from 'jquery'
+import slick from 'slick-carousel'
+
+$('#slider').slick({
+    infinite: true,
+    autoplay: true,
+    arrows: false,
+});
 
 if (process.env.NODE_ENV !== 'production') {
   require('./../index.pug')
