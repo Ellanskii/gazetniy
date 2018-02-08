00fc66f413703b50aa39b3dd6f5f96b32185b882
Elijah Ellanski <nameimprobable@gmail.com>
2018-02-08 23:08:43 +0300

    slider added

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -2,7 +2,7 @@
 import './../styles/variables-bulma.sass'
 import './../styles/main.scss'
 import './../styles/slick-carousel/slick/slick.scss';
-//import './../styles/slick-carousel/slick/slick-theme.scss';
+import './../styles/slick-carousel/slick/slick-theme.scss';
 
 import $ from 'jquery'
 import slick from 'slick-carousel'
@@ -12,6 +12,7 @@ $('#slider').slick({
     autoplay: true,
     arrows: false,
 });
+$('#overview-slider').slick();
 
 if (process.env.NODE_ENV !== 'production') {
   require('./../index.pug')
